910beb5f9a5853dbc9a0dc7bd854c7570b43dcdb
Kasper Kodura <127665338+kasserolee@users.noreply.github.com>
2025-05-23 18:43:18 +0200

    p4

---
+++ p4.py
@@ -0,0 +1,20 @@
+import numpy as np
+from mealpy.human_based.BRO import DevBRO
+from mealpy import FloatVar
+
+def fitness_func(solution):
+    N=solution.size
+    return 10*N+np.sum(xi**2-10*np.cos(2*np.pi*xi) for xi in solution)
+
+problem_dict = {
+    "bounds": FloatVar(lb = -5.12, ub = 5.12),
+    "obj_func": fitness_func,
+    "minmax": "min"
+}
+
+epoch = 100
+pop_size = 10
+threshold = 3
+
+model = DevBRO(epoch, pop_size, threshold)
+best = model.solve(problem_dict)
